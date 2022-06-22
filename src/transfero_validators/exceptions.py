3b7f0ea4182993c18d3f8a18679f20b23ee36af0
GabrielAureo <gabrielaureo@gmail.com>
2022-06-22 20:52:57 -0300

    exceptions

---
+++ src/transfero_validators/exceptions.py
@@ -0,0 +1,14 @@
+class EmailInvalidoException(Exception):
+    pass
+
+class CPFInvalidoException(Exception):
+    pass
+
+class CelularInvalidoException(Exception):
+    pass
+
+class SenhaCurtaException(Exception):
+    pass
+
+class SenhaFracaException(Exception):
+    pass
